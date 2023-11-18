788ced2dbe8cc4286fae8dd240acf7068e5f591a
dhaneshsaini <sainidhanesh58@gmail.com>
2023-11-18 09:43:10 +0530

    update

--- public/analytics/index.php
+++
@@ -1,5 +0,0 @@
-<html>
-   
-    <body>
-<iframe src="https://app.usermaven.com/share/UM8QOnkmbd/web" width="100%" height="100%" frameborder="0" loading="lazy"></iframe>        
-</body>
--- public/enquire/data.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli=new mysqli('localhost','brandingpioneers_enquirelannding','~daoqDBs=i[#','brandingpioneers_enquirelannding');
-if($mysqli->connect_error)
-{
-    echo $mysqli->connect_error;
-}
-
-?>
